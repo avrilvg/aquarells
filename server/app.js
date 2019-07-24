34a23c865c0a0b6981d2766cacaa34828d5906d0
avril <amira.abril@gmail.com>
2019-07-24 03:24:25 -0300

    change db

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 8080;
 
 // Set up mongoose connection
 const mongoose = require('mongoose');
-let dev_db_url = 'mongodb://localhost:27017/acuarelas';
+let dev_db_url = 'mongodb://root:aquas@localhost:27017/aquas';
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
